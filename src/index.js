046a8daa86fe44d66d48843704f1edd2c07e0112
kalkM <yidreslekali@gmail.com>
2023-07-20 22:36:29 +0300

    final

--- src/index.js
+++ src/index.js
@@ -2,27 +2,42 @@ function displayWeather(response) {
   weatherType.innerHTML = response.data.weather[0].description;
   cityElement.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
   tempElement.innerHTML = Math.round(response.data.main.temp);
-  celsiusTemperature = response.data.main.temp;
   humidityElement.innerHTML = `${response.data.main.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
-
+  celsiusTemperature = response.data.main.temp;
   weatherIconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   weatherIconElement.setAttribute("alt", response.data.weather[0].description);
 }
-function currentCity(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
+
+function searchCity(city) {
+  let apiKey = "f18833d4f810e7c8cfb463dcc107c093";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
 }
 
 function searchLocation(position) {
   let apiKey = "f18833d4f810e7c8cfb463dcc107c093";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
   axios.get(apiUrl).then(displayWeather);
 }
+
+function currentCity(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.getElementById("city-input").value.trim().toLowerCase();
+  searchCity(city);
+}
+
+let searchForm = document.getElementById("search-form");
+searchForm.addEventListener("submit", search);
+
 let currentCityForm = document.getElementById("current-location");
 currentCityForm.addEventListener("click", currentCity);
 
@@ -55,6 +70,8 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 let cityElement = document.getElementById("city");
 let weatherType = document.getElementById("weather-type");
 let tempElement = document.getElementById("temperature");
+let weatherIconElement = document.getElementById("weather-icon");
 let windElement = document.getElementById("wind");
 let humidityElement = document.getElementById("humidity");
-let weatherIconElement = document.getElementById("weather-icon");
+
+searchCity("Addis Ababa");
